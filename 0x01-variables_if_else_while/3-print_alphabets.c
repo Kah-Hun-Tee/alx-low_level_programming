9a06e307468b0f12fae067ab8af5043fcdda2051
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-14 17:04:46 -0700

    Mandatory Tasks

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int nam;
+
+	for (nam = 'a'; nam <= 'z'; nam++
+			putchar(nam);
+	for (nam = 'A'; nam <= 'Z'; nam++
+	           	putchar(nam);
+	putchar('\n');
+	return (0);
+}
