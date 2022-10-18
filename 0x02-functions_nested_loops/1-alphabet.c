349f820b0e93383c2b86a19a176d7091e1a69411
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-18 15:49:07 -0700

    Mandatory Tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int nam;
+
+	for (nam = 'a'; nam <= 'z'; nam++)
+		_putchar(nam);
+	_putchar('\n');
+}
