52d9e56afca8a621a6b4bb09eb7749d80468f96c
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-18 16:11:47 -0700

    Mandatory Tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints ten times the alphabet,
+ * in lowercase, followed by a new line.
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char t;
+	int o;
+
+	for (o = 0; o < 10; 0++)
+	{
+		for (t = 'a'; t <= 'z'; t++)
+			_putchar(t);
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character.
+ * @c: An input character
+ * Return: 1 if c is lowercase, 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	char t;
+	int lower = 0;
+
+	for (t = 'a', t <= 'z'; t++)
+	{
+		if (t == c)
+			lower = 1;
+	}
+	return (lower);
+}
