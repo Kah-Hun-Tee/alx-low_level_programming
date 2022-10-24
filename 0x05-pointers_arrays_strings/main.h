3d7438db468e3b11721c80a725a71453006a740c
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-24 15:20:17 -0700

    Header File

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define FILE_MAIN
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+void reset_to_98(int *q);
+void swap_int(int *p, int *q);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
