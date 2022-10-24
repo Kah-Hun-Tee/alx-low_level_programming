9a35bee213b3788ff830479673949f4528de471c
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-24 16:34:46 -0700

    Mandatory Tasks

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - this code prints n element of an array of integers,
+ * followed by a new line
+ * @p: array to be printed
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *p, int n)
+{
+	int q;
+
+	for (q = 0; q < n; q++)
+	{
+		if (q == 0)
+			printf("%d", p[q]);
+		else
+			printf(", %d", p[q]);
+	}
+	printf('\n');
+}
