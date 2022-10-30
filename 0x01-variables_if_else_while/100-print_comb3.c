d0a461a05a0cb926e6006b20e2a66a82080e2c45
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-29 17:14:37 -0700

    New

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,3 @@ int main(void)
 
 	for (p = 0; p < 9; p++)
 	{
-		for (q = p + 1; q < 10; q++)
-		{
-			putchar((p % 10) + '0');
-			putchar((q % 10) + '0');
-			if (p == 8 && q == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
-}
