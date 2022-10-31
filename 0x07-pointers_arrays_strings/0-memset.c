3dda7e8c9eb8174024927d06167c475f6622b95f
Kah-Hun-Tee <toonmynynoo279@gmail.com>
2022-10-31 13:39:09 -0700

    Mandatory Task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,19 @@
 /**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
- * @b: char to copy
+ * @p: char to copy
  * @n: number of times to copy b
  * Return: pointer to the memory area s
  */
+
+char *_memset(char *s, char p, unsigned int n)
+{
+	unsigned int q;
+
+	for (q = 0; q < n; q++)
+	{
+		s[q] = p;
+	}
+
+	return (s);
+}
